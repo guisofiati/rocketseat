e35ffbc146d3c979e6b5ed612643fa78ed8029e3
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-07 10:31:46 -0300

    Discover: EJS. Renderizando elementos dinamicamente com forEach

--- discover/beginner/nodejs/ejs/server.js
+++ discover/beginner/nodejs/ejs/server.js
@@ -1,18 +1,41 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 
 app.set("view engine", "ejs"); // ferramenta para renderizar html sera ejs
 
-// tem que estar dentro de uma pasta 'views' 
-app.get("/", function(req, res) {
-    res.render("pages/index"); 
+// tem que estar dentro de uma pasta 'views'
+app.get("/", function (req, res) {
+  const items = [
+    {
+      id: 1,
+      message: "Bom dia, otima pagina!",
+    },
+    {
+      id: 2,
+      message: "Estou aprendendo muito, obrigado.",
+    },
+    {
+      id: 3,
+      message: "Gostei :D",
+    },
+    {
+      id: 4,
+      message: "Tenho uma critica...",
+    },
+  ];
+
+  const subtitle = 'O EJS é uma linguagem de modelagem e criação de página HTML usando JS'
+  res.render("pages/index", {
+    messages: items,
+    subtitle: subtitle
+  });
 });
 
-app.get("/about", function(req, res) {
-    res.render("pages/about");
+app.get("/about", function (req, res) {
+  res.render("pages/about");
 });
 
 app.listen(8080);
 
 const interval = 10000;
-setInterval(() => console.log('listening server'), interval);
+setInterval(() => console.log("listening server"), interval);

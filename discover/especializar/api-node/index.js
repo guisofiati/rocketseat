fa707548080754631efad2586fb5ada969b5b470
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-12 10:21:34 -0300

    Discover: API. PUT method

--- discover/especializar/api-node/index.js
+++ discover/especializar/api-node/index.js
@@ -4,11 +4,14 @@ const app = express();
 
 app.listen("3000");
 
-//middleware -> ponte entre as reqs.
-// quando mando algo do postman para ca, primeiro ele passa pelo middleware e dps executa
+// middleware
 app.use(express.json());
 
-app.route("/").post((req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+let author = "John";
+
+app.route("/").put((req, res) => {
+    author = req.body.author; // pegar apenas o conteudo e nao a estrutura json '{}'
+    res.send(author);
 });
+
+app.route("/").get((req, res) => res.send(author));

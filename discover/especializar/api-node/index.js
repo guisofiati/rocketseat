88bf2fa99a648b8db4624100681bae2a8ddb16ec
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-12 14:58:53 -0300

    Discover: API. Consumindo API do Github

--- discover/especializar/api-node/index.js
+++ discover/especializar/api-node/index.js
@@ -1,9 +1,17 @@
+const axios = require("axios");
 const express = require("express");
 
 const app = express();
 
 app.listen("3000");
 
-//app.route("/").get((req, res) => res.send(req.query.nome)); // um campo
-app.route("/").get((req, res) => res.send(req.query));
-app.route("/about/user").get((req, res) => res.send(req.query));
+app.route("/").get((req, res) => {
+    axios
+        .get("https://api.github.com/users/guisofiati")
+        //.then((response) => res.send(response.data)) // all data
+        //.then((response) => res.send(response.data.avatar_url)) // avatar url
+        .then((response) =>
+            res.send(`<img src="${response.data.avatar_url}"/>`)
+        )
+        .catch((error) => console.log(error));
+});

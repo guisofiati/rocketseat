a99e2d89a2048ec47cdf0718d16955a636313b1d
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-12 09:59:21 -0300

    Discover: API. GET method

--- discover/especializar/api-node/index.js
+++ discover/especializar/api-node/index.js
@@ -3,3 +3,9 @@ const express = require("express");
 const app = express();
 
 app.listen("3000");
+
+app.route("/").get((req, res) => {
+    res.send("hello world");
+});
+
+app.route("/about").get((req, res) => res.send("page about"));

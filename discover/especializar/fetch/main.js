3b0f7ace801e9cb7e5c8de73a83d488e04971898
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-13 09:39:42 -0300

    Discover: Consumindo API com Fetch. Get by id

--- discover/especializar/fetch/main.js
+++ discover/especializar/fetch/main.js
@@ -1,10 +1,23 @@
 const url = "http://localhost:5500/api";
 
-async function getUsers() {
-    await fetch(url)
+function getUsers() {
+    fetch(url)
         .then((response) => response.json())
         .then((data) => (renderApiResults.textContent = JSON.stringify(data)))
         .catch((error) => console.error(error));
 }
 
+function getUserById() {
+    fetch(`${url}/1`)
+        .then((res) => res.json())
+        .then((data) => {
+            // data / data.avatar / data.city
+            userName.textContent = data.name;
+            userCity.textContent = data.city;
+            userAvatar.src = data.avatar;
+        })
+        .catch((e) => console.error(e));
+}
+
+getUserById();
 getUsers();

3a21257c10519965eb236d1be13cc7853233ac9a
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-14 15:09:20 -0300

    Discover: React. Introducao ao estado com a hook useState

--- discover/especializar/react/src/pages/Home/index.jsx
+++ discover/especializar/react/src/pages/Home/index.jsx
@@ -1,13 +1,21 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 import { Card } from "../../components/Card";
 
 export function Home() {
+    const [studentName, setStudentName] = useState(""); // valor inicial da var
     return (
         <>
             <div className="container">
                 <h1>Lista de Presença</h1>
-                <input type="text" placeholder="Digite um nome" />
+                <h2>Nome: {studentName}</h2>
+                <input
+                    type="text"
+                    placeholder="Digite um nome"
+                    onChange={(event) => setStudentName(event.target.value)}
+                    // pegando o conteudo atual do input, e atualizando o setStudentName
+                />
                 <button type="button">Adicionar</button>
                 <Card name="Jose da Silva" time="10:55:22" />
                 <Card name="Maria das Dores" time="11:31:02" />

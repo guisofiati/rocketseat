27ab04a87595b493efdb50767dce9b1ee6b7ce7e
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-07-14 10:32:17 -0300

    Discover: React. Adicionando CSS na pagina

--- discover/especializar/react/src/pages/Home/index.jsx
+++ discover/especializar/react/src/pages/Home/index.jsx
@@ -3,9 +3,11 @@ import "./styles.css";
 export function Home() {
     return (
         <>
-            <h1>Lista de Presença</h1>
-            <input type="text" placeholder="Digite um nome" />
-            <button type="button">Adicionar</button>
+            <div className="container">
+                <h1>Lista de Presença</h1>
+                <input type="text" placeholder="Digite um nome" />
+                <button type="button">Adicionar</button>
+            </div>
         </>
     );
 }

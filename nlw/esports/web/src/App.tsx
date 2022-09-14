454e621cbfe1460ad1fe1133e39d7d0334c584b5
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-14 11:31:58 -0300

    NLW eSports: Aula 02 web. Instalando tailwindcss no projeto

--- nlw/esports/web/src/App.tsx
+++ nlw/esports/web/src/App.tsx
@@ -1,20 +1,7 @@
-interface ButtonProps {
-    title: string;
-}
-
-function Button(props: ButtonProps) {
-    return <button>{props.title}</button>;
-}
+import "./styles/main.css";
 
 function App() {
-    return (
-        <div>
-            <Button title="Send 1" />
-            <Button title="Send 2" />
-            <Button title="Send 3" />
-            <Button title="Hello World" />
-        </div>
-    );
+    return <div className="w-8 h-8 bg-black dark:bg-green-500"></div>; // muda pra verde se tiver theme dark ativado
 }
 
 export default App;
---
+++ nlw/esports/web/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.tsx"], // onde vao estar os arquivos html que vao ter classes do tailwind
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};

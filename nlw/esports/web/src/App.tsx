8f279065a8d9376e9a5da2628075892035e44244
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-14 11:53:13 -0300

    NLW eSports: Aula 02 web. Titulo e logo

--- nlw/esports/web/src/App.tsx
+++ nlw/esports/web/src/App.tsx
@@ -1,7 +1,21 @@
 import "./styles/main.css";
 
+import LogoImg from "./assets/logo-nlw-esports.svg";
+
 function App() {
-    return <div className="w-8 h-8 bg-black dark:bg-green-500"></div>; // muda pra verde se tiver theme dark ativado
+    return (
+        <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
+            <img src={LogoImg} alt="Logo do NLW" />
+
+            <h1 className="text-6xl text-white font-black mt-20">
+                Seu{" "}
+                <span className="bg-nlw-gradient text-transparent bg-clip-text">
+                    duo
+                </span>{" "}
+                está aqui.
+            </h1>
+        </div>
+    );
 }
 
 export default App;
--- nlw/esports/web/tailwind.config.cjs
+++ nlw/esports/web/tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.tsx"], // onde vao estar os arquivos html que vao ter classes do tailwind
+    content: ["./src/**/*.tsx", "./index.html"], // onde vao estar os arquivos html que vao ter classes do tailwind
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                galaxy: "url('/background-galaxy.png')",
+                "nlw-gradient":
+                    "linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)",
+            },
+        },
     },
     plugins: [],
 };

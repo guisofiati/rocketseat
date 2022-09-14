7d36097833c40c87e7e9fa99f509d8cb83ccda9f
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-14 16:37:07 -0300

    NLW eSports: Aula 02 mobile. Titulo e subtitulo

--- nlw/esports/mobile/src/screens/Home/index.tsx
+++ nlw/esports/mobile/src/screens/Home/index.tsx
@@ -1,7 +1,23 @@
-import { View } from "react-native";
+import { Image, View } from "react-native";
+
+import LogoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
 
 import { styles } from "./styles";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={LogoImg}
+        defaultSource={LogoImg}
+        style={styles.logo}
+      ></Image>
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
 }

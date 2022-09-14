434c234f3d7a220d8fdc9ea1ed18481104957d33
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-14 17:20:23 -0300

    NLW eSports: Aula 02 mobile. Lista de jogos

--- nlw/esports/mobile/src/screens/Home/index.tsx
+++ nlw/esports/mobile/src/screens/Home/index.tsx
@@ -1,7 +1,10 @@
-import { Image, View } from "react-native";
+import { Image, View, FlatList } from "react-native";
 
 import LogoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
+
+import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
@@ -17,6 +20,15 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );

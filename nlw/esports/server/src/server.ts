799ef0242619a54f3674cbb6532e253948ba198b
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-15 09:42:40 -0300

    NLW eSports: Aula 03 backend. Prisma ORM e tabela de Games

--- nlw/esports/server/src/server.ts
+++ nlw/esports/server/src/server.ts
@@ -1,12 +1,24 @@
-// const express = require("express"); -> type: commonjs
-
-import express from "express"; // -> type: module .mjs
+import express from "express";
 
 const app = express();
 
 app.listen(3333);
 
-app.get("/ads", (req, res) => {
+app.get("/games", (req, res) => {
+    return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+    // const adId = req.params.adId;
+    //res.location("/ads" + )
+    return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+    // listar ads POR jogo
+
+    //const gameId = req.params.id;
+
     return res.json([
         { id: 1, name: "Anuncio 1" },
         { id: 2, name: "Anuncio 2" },
@@ -14,4 +26,10 @@ app.get("/ads", (req, res) => {
         { id: 4, name: "Anuncio 4" },
         { id: 5, name: "Anuncio 5" },
     ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+    //const adId = req.params.id;
+
+    return res.json([]);
 });

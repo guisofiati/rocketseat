98ae9061ec4db5e983b227558feaffb6ae1a5a6b
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-16 15:53:01 -0300

    NLW eSports: Aula 04 web. Form modal

--- nlw/esports/web/src/components/CreateAdBanner.tsx
+++ nlw/esports/web/src/components/CreateAdBanner.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function CreateAdBanner() {
     return (
@@ -12,10 +13,12 @@ export function CreateAdBanner() {
                         Publique um anúncio para encontrar novos players!
                     </span>
                 </div>
-                <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-                    <MagnifyingGlassPlus size={24} />
-                    Publicar anúncio
-                </button>
+                <Dialog.Trigger>
+                    <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+                        <MagnifyingGlassPlus size={24} />
+                        Publicar anúncio
+                    </button>
+                </Dialog.Trigger>
             </div>
         </div>
     );

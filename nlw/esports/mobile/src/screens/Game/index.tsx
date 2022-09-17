167ed687ae19f91a8cecbc338039311a5157f779
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-17 00:15:23 -0300

    NLW eSports: Aula 04 mobile. Game screen e baner de anuncios

--- nlw/esports/mobile/src/screens/Game/index.tsx
+++ nlw/esports/mobile/src/screens/Game/index.tsx
@@ -1,17 +1,21 @@
-import { View, TouchableOpacity, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { View, TouchableOpacity, Image, FlatList, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GameParams } from "../../@types/navigation";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
 
-import LogoImg from "../../assets/logo-nlw-esports.png";
 import { Background } from "../../components/Background";
 import { Heading } from "../../components/Heading";
+import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import LogoImg from "../../assets/logo-nlw-esports.png";
 
 import { THEME } from "../../theme";
 import { styles } from "./styles";
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
@@ -19,6 +23,12 @@ export function Game() {
   function handleBackPreviousScreen() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.15.16:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  }, []);
 
   return (
     <Background>
@@ -35,7 +45,32 @@ export function Game() {
           <View style={styles.rightLogo} />
         </View>
 
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
         <Heading title={game.title} subtitle="Conecte-se e começe a jogar!" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[
+            duos.length > 0 ? styles.contentList : styles.emptyListContent,
+          ]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   );

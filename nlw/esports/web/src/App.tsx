666c9813b72b6fc08fcbddc73c8cb4bc1cca0ee8
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-09-18 16:04:01 -0300

    NLW eSports: Aula 05 web. Fazendo req para a api

--- nlw/esports/web/src/App.tsx
+++ nlw/esports/web/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { GamerBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 import { CreateAdModal } from "./components/CreateAdModal";
+import axios from "axios";
 import * as Dialog from "@radix-ui/react-dialog";
 
 import LogoImg from "./assets/logo-nlw-esports.svg";
@@ -19,11 +20,9 @@ function App() {
     const [games, setGames] = useState<Game[]>([]);
 
     useEffect(() => {
-        fetch("http://localhost:3333/games")
-            .then((response) => response.json())
-            .then((data) => {
-                setGames(data);
-            });
+        axios("http://localhost:3333/games").then((response) => {
+            setGames(response.data);
+        });
     }, []);
 
     return (

aebc38c240a1866dda8f9f28ca66831141ddd21b
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-10-12 21:39:08 -0300

    Ignite Lab: Aula 02. Adicionando e configurando o tailwindcss

---
+++ ignite-lab/design-system/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // onde estao os arquivos de conteudo da aplicação que sera usado o tailwindcss
+  content: [
+    "./src/**/*.tsx", // pasta src em qualquer pasta e qualquer arquivo .tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

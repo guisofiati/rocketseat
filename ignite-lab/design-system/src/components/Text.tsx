d40dec6e965647b7797e99b36dacefcb6e2cfd36
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-10-14 18:56:17 -0300

    Ignite Lab: Aula 03. Criando o formulario com react

--- ignite-lab/design-system/src/components/Text.tsx
+++ ignite-lab/design-system/src/components/Text.tsx
@@ -6,20 +6,25 @@ export interface TextProps {
     size?: "sm" | "md" | "lg"; // ?: = optional in typescript
     children: ReactNode; // qualquer coisa (componente, tag, etc)
     asChild?: boolean;
+    className?: string; // adições de className
 }
 
 // caso nao definido, o size padrao sera md (sm)
-export function Text({ size = "md", children, asChild }: TextProps) {
+export function Text({ size = "md", children, asChild, className }: TextProps) {
     // permite que o user escolha qual tag utilizar o Text, por padrao é a span
     const Comp = asChild ? Slot : "span";
 
     return (
         <Comp
-            className={clsx("text-gray-100 font-sans", {
-                "text-xs": size === "sm", // pegando nossas confs do tailwind
-                "text-sm": size === "md",
-                "text-md": size === "lg",
-            })}
+            className={clsx(
+                "text-gray-100 font-sans",
+                {
+                    "text-xs": size === "sm", // pegando nossas confs do tailwind
+                    "text-sm": size === "md",
+                    "text-md": size === "lg",
+                },
+                className
+            )}
         >
             {children}
         </Comp>

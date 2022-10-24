db18a8694535064062e478f7a7d1339e2a0e0803
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-10-24 14:57:17 -0300

    Ignite Node: Chapter 1. FinAPI - POST nova conta

--- ignite/node/chapter1/finapi/src/index.js
+++ ignite/node/chapter1/finapi/src/index.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.listen(3333);
+
+// middlewares
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { name, cpf } = request.body;
+
+    const id = uuidv4();
+
+    customers.push({
+        id,
+        name,
+        cpf,
+        statements: []
+    });
+
+    return response.status(201).send();
+});

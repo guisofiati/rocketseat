f92613cb491f60416ba8c34e14c198e117b8fc64
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-10-25 10:44:52 -0300

    Ignite Node: Chapter 1. Desafio conceitos do node - CRUD completo

---
+++ ignite/node/chapter1/desafio-conceitos-nodejs/README.md
@@ -0,0 +1,9 @@
+# Desafio principal - Conceitos do Node.js
+
+## Requisitos
+
+- Criar um novo _todo_;
+- Listar todos os _todos_;
+- Alterar o `title` e `deadline` de um _todo_ existente;
+- Marcar um _todo_ como feito;
+- Excluir um _todo_;
---
+++ ignite/node/chapter1/desafio-conceitos-nodejs/src/index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+  
+  if (!user) {
+    response.status(404).json({ error: "User not found." });
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const isAccountAlreadyCreated = users.some(user => user.username === username);
+
+  if (isAccountAlreadyCreated) {
+    response.status(400).json({ error: "Account already exists." });
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+  return response.status(201).send(); // retornar o user aqui
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  user.todos.push({
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  });
+
+  response.status(201).send(); // retornar a todo aqui
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    response.status(404).json({ error: "Todo not found." });
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.send();
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    response.status(404).json({ error: "Todo not found." });
+  }
+
+  todo.done = true;
+
+  return response.send();
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    response.status(404).json({ error: "Todo not found." });
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
+});
+
+module.exports = app;

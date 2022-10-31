85477c17e277dda365160f169d32f6d621593adb
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-10-31 10:40:09 -0300

    Ignite Node: Chapter 2. Intro ao typescript

--- ignite/node/chapter2/intro-typescript/src/CreateCarService.ts
+++ ignite/node/chapter2/intro-typescript/src/CreateCarService.ts
@@ -4,8 +4,17 @@
  * year: number
  */
 
+interface Car {
+  company: string;
+  country: string;
+  year: number;
+}
+
 class CreateCarService {
-  execute(company: string, country: string, year: number) {
+  // execute(data: Car) {
+  //   console.log(data.company, data.country, data.year); -> onde for chamado é so passar os argumentos
+  // }
+  execute({ country, year, company }: Car) {
     console.log(company, country, year);
   }
 }
--- ignite/node/chapter2/intro-typescript/src/routes.ts
+++ ignite/node/chapter2/intro-typescript/src/routes.ts
@@ -3,7 +3,11 @@ import CreateCarService from "./CreateCarService";
 
 // colocando o conteudo em arquivo separado, para nao poluir o server.ts
 export function createCar(request: Request, response: Response) {
-  CreateCarService.execute("Toyota", "Japan", 2007); // "2007" -> erro
+  CreateCarService.execute({
+    year: 2007,
+    company: "Toyota",
+    country: "Japan",
+  }); // "2007" -> erro
 
   return response.send();
 }

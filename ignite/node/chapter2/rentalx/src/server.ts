97be26c75132a704d74c46ed623a62923f19c04b
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-11-02 15:26:46 -0300

    Ignite Node: Chapter 2. RentalX API. Removendo dependencias e diagrama do negocio

--- ignite/node/chapter2/rentalx/src/server.ts
+++ ignite/node/chapter2/rentalx/src/server.ts
@@ -4,15 +4,4 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello, world!" });
-});
-
-// test debug
-// app.post("/cars", (request, response) => {
-//   const { name } = request.body;
-
-//   return response.json(name);
-// });
-
 app.listen(3333, () => console.log("Server running..."));

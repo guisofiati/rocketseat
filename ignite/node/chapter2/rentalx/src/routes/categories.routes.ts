aa380be53cf7569f50b16db94c6966d2b5f69725
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-11-02 16:12:34 -0300

    Ignite Node: Chapter 2. RentalX API. Criando primeiro recurso

---
+++ ignite/node/chapter2/rentalx/src/routes/categories.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { v4 as uuid } from "uuid";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const category = {
+    uuid: uuid(),
+    name,
+    description,
+    created_at: new Date(),
+  };
+
+  categories.push(category);
+
+  return response.status(201).json(category);
+});
+
+export { categoriesRoutes };

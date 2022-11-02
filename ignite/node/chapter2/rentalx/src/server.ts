de2bdcc084ff4298b723fa9deb74d6ce0205a2b9
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-11-02 11:12:18 -0300

    Ignite Node: Chapter 2. RentalX API. Testando debug

--- ignite/node/chapter2/rentalx/src/server.ts
+++ ignite/node/chapter2/rentalx/src/server.ts
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello, world!" });
 });
+
+// test debug
+// app.post("/cars", (request, response) => {
+//   const { name } = request.body;
+
+//   return response.json(name);
+// });
 
 app.listen(3333, () => console.log("Server running..."));

ad8b70de1cc7c9c0e303431240fc31093602882e
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-11-07 14:57:29 -0300

    Ignite Node: Chapter 2. RentalX API. Lendo dados atraves de upload de arquivo com a lib multer

--- ignite/node/chapter2/rentalx/src/routes/category.routes.ts
+++ ignite/node/chapter2/rentalx/src/routes/category.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/car/useCases/createCategory";
 import { listCategoriesController } from "../modules/car/useCases/listCategories";
 
 const categoryRoutes = Router();
+
+const upload = multer({
+  dest: "./temp",
+});
 
 categoryRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +16,12 @@ categoryRoutes.post("/", (request, response) => {
 
 categoryRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoryRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
 
 export { categoryRoutes };

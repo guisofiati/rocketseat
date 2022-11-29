d05d15c1c6b9025d4160fdb8be9b51eae69b84e6
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-11-29 10:15:15 -0300

    Ignite Node: Chapter 4. Jest

---
+++ ignite/node/chapter2/rentalx/src/modules/car/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create category", () => {
+  it("Expected 2 + 2 be 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("Expected 2 + 2 not be 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});

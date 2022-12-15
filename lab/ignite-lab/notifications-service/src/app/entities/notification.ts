85e0ccaa66243ddf1d4ec02a3da745f14b870c72
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-12-15 19:12:36 -0300

    Ignite Lab: Aula 03. Controllers

--- lab/ignite-lab/notifications-service/src/app/entities/notification.ts
+++ lab/ignite-lab/notifications-service/src/app/entities/notification.ts
@@ -18,8 +18,11 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = uuid();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? uuid();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
--- lab/ignite-lab/notifications-service/src/infra/http/http.module.ts
+++ lab/ignite-lab/notifications-service/src/infra/http/http.module.ts
@@ -1,4 +1,9 @@
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@app/use-cases/read-notification';
 import { SendNotification } from '@app/use-cases/send-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationController } from './controllers/notification.controller';
@@ -6,6 +11,13 @@ import { NotificationController } from './controllers/notification.controller';
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}

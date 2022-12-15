1576405932b160831525e9d35bcfa8a460ac7968
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-12-15 13:51:38 -0300

    Ignite Lab: Aula 03. Arrumando paths

--- lab/ignite-lab/notifications-service/src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ lab/ignite-lab/notifications-service/src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -1,6 +1,6 @@
+import { Notification } from '@app/entities/notification';
+import { NotificationRepository } from '@app/repositories/notification-repository';
 import { Injectable } from '@nestjs/common';
-import { Notification } from '../../../../app/entities/notification';
-import { NotificationRepository } from '../../../../app/repositories/notification-repository';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()

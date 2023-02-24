a30400f58dcdb4f22e169a63d3d5e440a0424829
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-02-24 15:32:02 -0300

    Ignite Node 2023. Migrations e testando queries

--- ignite/node-2023/api-rest-fastify/src/server.ts
+++ ignite/node-2023/api-rest-fastify/src/server.ts
@@ -1,12 +1,23 @@
 import fastify from "fastify";
 import { knex } from "./database";
+// import { randomUUID as uuid } from "node:crypto";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  // const transaction = await knex("transactions")
+  //   .insert({
+  //     id: uuid(),
+  //     title: "Transação de teste",
+  //     amount: 1000,
+  //   })
+  //   .returning("*");
 
-  return tables;
+  const transactions = await knex("transactions")
+    .where("amount", 1000)
+    .select("*");
+
+  return transactions;
 });
 
 app

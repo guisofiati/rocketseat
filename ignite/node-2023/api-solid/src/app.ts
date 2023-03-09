3f27de31847a179d941b38b52c32858507016f78
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-03-09 11:19:17 -0300

    Ignite Node 2023. Setup Prisma e Docker Compose

--- ignite/node-2023/api-solid/src/app.ts
+++ ignite/node-2023/api-solid/src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "gui",
+    email: "guest@prisma.com",
+  },
+});
 
 export const app = fastify();

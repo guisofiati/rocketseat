b038ccd03d036050078854775fd0dbb985a578fd
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-03-10 09:49:14 -0300

    Ignite Node 2023. Inversao de dependencias

--- ignite/node-2023/api-solid/src/http/controllers/register.ts
+++ ignite/node-2023/api-solid/src/http/controllers/register.ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/lib/prisma";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
-import { hash } from "bcryptjs";
+import { RegisterUseCase } from "@/use-cases/register";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,25 +12,18 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
 
-  const userAlreadyExists = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userAlreadyExists) {
-    return reply.status(409).send();
-  }
-
-  await prisma.user.create({
-    data: {
+    await registerUseCase.execute({
       name,
       email,
-      password_hash,
-    },
-  });
+      password,
+    });
+  } catch (err) {
+    return reply.status(409).send();
+  }
 
   return reply.status(201).send();
 }
---
+++ ignite/node-2023/api-solid/src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6);
+
+    const userAlreadyExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("E-mail already exists");
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+  }
+}

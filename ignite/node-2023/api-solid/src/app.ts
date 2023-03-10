3839bceb8220d7c29be2c1718fecf35b66fdeab3
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-03-10 07:49:35 -0300

    fix

--- ignite/node-2023/api-solid/src/app.ts
+++ ignite/node-2023/api-solid/src/app.ts
@@ -1,13 +1,3 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    name: "gui",
-    email: "guest@prisma.com",
-  },
-});
 
 export const app = fastify();

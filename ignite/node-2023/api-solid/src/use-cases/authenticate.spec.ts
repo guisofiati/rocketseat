791787173721f2623c56bacc8458aa9b5b8cc66f
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-03-14 11:09:21 -0300

    Ignite Node 2023. Autenticacao e factory pattern

---
+++ ignite/node-2023/api-solid/src/use-cases/authenticate.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { hash } from "bcryptjs";
+import { beforeEach, describe, expect, it } from "vitest";
+import { AuthenticateUseCase } from "./authenticate";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: AuthenticateUseCase;
+
+describe("Authenticate use case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(usersRepository);
+  });
+
+  it("it should be able to authenticate", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "john@gmail.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { user } = await sut.execute({
+      email: "john@gmail.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("it should not be able to authenticate with wrong email", async () => {
+    expect(() =>
+      sut.execute({
+        email: "john@gmail.com",
+        password: "123456",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("it should not be able to authenticate with wrong password", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "john@gmail.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    expect(() =>
+      sut.execute({
+        email: "john@gmail.com",
+        password: "654321",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+});
--- ignite/node-2023/api-solid/src/use-cases/register.spec.ts
+++ ignite/node-2023/api-solid/src/use-cases/register.spec.ts
@@ -1,15 +1,20 @@
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { compare } from "bcryptjs";
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { RegisterUseCase } from "./register";
 
-describe("Register use case", () => {
-  it("it should be able to register", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+let usersRepository: InMemoryUsersRepository;
+let sut: RegisterUseCase;
 
-    const { user } = await registerUseCase.execute({
+describe("Register use case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new RegisterUseCase(usersRepository);
+  });
+
+  it("it should be able to register", async () => {
+    const { user } = await sut.execute({
       name: "John Doe",
       email: "john@gmail.com",
       password: "123456",
@@ -19,10 +24,7 @@ describe("Register use case", () => {
   });
 
   it("should hash user password upon registration", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
-
-    const { user } = await registerUseCase.execute({
+    const { user } = await sut.execute({
       name: "John Doe",
       email: "john@gmail.com",
       password: "123456",
@@ -39,19 +41,16 @@ describe("Register use case", () => {
   });
 
   it("should not be able to register with email in use", async () => {
-    const usersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
-
     const email = "john@gmail.com";
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: "John Doe",
       email,
       password: "123456",
     });
 
     await expect(() =>
-      registerUseCase.execute({
+      sut.execute({
         name: "John Doe",
         email,
         password: "123456",

4a607f6d4dd191a7be06507fff3e536be7237433
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-03-23 10:53:26 -0300

    Ignite Node 2023. Rotas e testes e2e

--- ignite/node-2023/api-solid/src/use-cases/factories/fetch-nearby-gyms-use-case.ts
+++ ignite/node-2023/api-solid/src/use-cases/factories/fetch-nearby-gyms-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository";
-import { SearchGymsUseCase } from "../search-gyms";
+import { FetchNearbyGymsUseCase } from "../fetch-nearby-gyms";
 
 export function makeFetchNearbyGymsUseCase() {
   const gymsRepository = new PrismaGymsRepository();
-  const useCase = new SearchGymsUseCase(gymsRepository);
+  const useCase = new FetchNearbyGymsUseCase(gymsRepository);
 
   return useCase;
 }

885752ad281cb89bb00332c5216b36c409ca7692
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-03-24 10:21:52 -0300

    Ignite Node 2023. Refresh token

--- ignite/node-2023/api-solid/src/http/controllers/users/routes.ts
+++ ignite/node-2023/api-solid/src/http/controllers/users/routes.ts
@@ -3,9 +3,11 @@ import { authenticate } from "./authenticate";
 import { profile } from "./profile";
 import { register } from "./register";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+  app.patch("/token/refresh", refresh);
   app.get("/me", { onRequest: [verifyJWT] }, profile);
 }

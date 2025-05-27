de4f6fc886f337e405150b66cfc3ac52733b8f3a
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-05-27 16:33:28 -0300

    react native: project structure

--- ignite/react-native/comprar/src/app/Home/index.tsx
+++ ignite/react-native/comprar/src/app/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
 import { styles } from "./styles";
 
-export default function App() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello, World!</Text>

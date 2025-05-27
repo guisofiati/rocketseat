e8f4663851e3785e3e0196b7f77350a27e9c0bbb
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-05-27 15:31:01 -0300

    react native: intro and styles

---
+++ ignite/react-native/comprar/App.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  )
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center"
+//   },
+//   text: {
+//     fontSize: 32,
+//     fontWeight: "bold",
+//     color: "blue"
+//   }
+// })

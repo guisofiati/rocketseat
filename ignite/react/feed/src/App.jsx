5e5096c8f4c5b9d391a923cac107100e4b7bcca4
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-05-27 11:16:55 -0300

    react: sidebar component

--- ignite/react/feed/src/App.jsx
+++ ignite/react/feed/src/App.jsx
@@ -2,19 +2,28 @@ import { Post } from "./Post"
 import { Header } from "./components/Header"
 
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
+
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Guilherme Sofiati"
-        content="Lorem ipsum dolor sit amet"
-      />
-      <Post
-        author="Giovanni Sofiati"
-        content="Lorem ipsum dolor sit amet"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Guilherme Sofiati"
+            content="Lorem ipsum dolor sit amet"
+          />
+          <Post
+            author="Giovanni Sofiati"
+            content="Lorem ipsum dolor sit amet"
+          />
+        </main>
+      </div>
     </>
   )
 }

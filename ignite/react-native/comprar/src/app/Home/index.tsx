e7e337729f1d9fa458530636c764f1c1ef877c16
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-05-28 18:01:45 -0300

    react native: input component

--- ignite/react-native/comprar/src/app/Home/index.tsx
+++ ignite/react-native/comprar/src/app/Home/index.tsx
@@ -1,13 +1,14 @@
 import { View, Image } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
-  //       <Image source={require("../../assets/logo.png")} style={styles.logo} />
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
-      <Button title="Adicionar" onPress={() => console.log("test")} />
+      <Input placeholder="O que você precisa comprar?" />
+      <Button title="Adicionar" />
     </View>
   )
 }

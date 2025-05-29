a1eaf5c09e0693860068cc5902a7c78b6ef6a96d
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-05-29 10:59:48 -0300

    react native: Filter and StatusIcon components and icons

--- ignite/react-native/comprar/src/app/Home/index.tsx
+++ ignite/react-native/comprar/src/app/Home/index.tsx
@@ -2,13 +2,21 @@ import { View, Image } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Adicionar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Adicionar" />
+      </View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.PENDING} isActive />
+        <Filter status={FilterStatus.DONE} isActive={false} />
+      </View>
     </View>
   )
 }

0e2c6545680da8a88c0d0ae09fe7a45869105eee
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-05-29 10:09:40 -0300

    react: props and iteration

--- ignite/react/feed/src/App.jsx
+++ ignite/react/feed/src/App.jsx
@@ -5,22 +5,61 @@ import { Sidebar } from "./components/Sidebar"
 import { Post } from "./components/Post"
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/FlavioHenriqueF.png",
+      name: "Flavio Henrique",
+      role: "Desenvolvedor Front-End"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#nlw" },
+      { type: "link", content: "#rocketseat" },
+    ],
+    publishedAt: new Date("2025-05-29 08:00:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/GiovanniSofiati.png",
+      name: "Giovanni Sofiati",
+      role: "Programador"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#nlw" },
+      { type: "link", content: "#rocketseat" },
+    ],
+    publishedAt: new Date("2025-05-29 08:10:00")
+  },
+]
+
+
 export function App() {
   return (
     <>
       <Header />
-
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Guilherme Sofiati"
-            content="Lorem ipsum dolor sit amet"
-          />
-          <Post
-            author="Giovanni Sofiati"
-            content="Lorem ipsum dolor sit amet"
-          />
+          {posts.map(post => {
+            console.log(post);
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>

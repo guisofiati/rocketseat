38010b50c9df8fd3dad567cb0467ade8442e4c0c
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-02 14:59:38 -0300

    react native: status state

--- ignite/react-native/comprar/src/app/Home/index.tsx
+++ ignite/react-native/comprar/src/app/Home/index.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
+import { useState } from "react";
 
 const FILTER_STATUS: FilterStatus[] = [
   FilterStatus.PENDING,
@@ -19,39 +20,36 @@ const ITEMS = [
 ]
 
 export function Home() {
+  const [filter, setFilter] = useState<FilterStatus>(FilterStatus.PENDING)
+  const [description, setDescription] = useState("")
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
+        <Input placeholder="O que você precisa comprar?" onChangeText={setDescription} />
         <Button title="Adicionar" />
       </View>
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map(status => {
-            return <Filter key={status} status={status} isActive />
+            return (
+              <Filter
+                key={status}
+                status={status}
+                isActive={status === filter}
+                onPress={() => setFilter(status)}
+              />)
           })}
           <TouchableOpacity style={styles.clearButton} activeOpacity={0.3}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
-        {/* <ScrollView>
-          {ITEMS.map((value) => (
-            <Item
-              key={value}
-              data={{ status: FilterStatus.DONE, description: value.toString() }}
-              onStatusChange={() => console.log('changed')}
-              onRemove={() => console.log('removed')}
-            />
-          ))}
-        </ScrollView> */}
         <FlatList
           data={ITEMS}
           keyExtractor={item => item.id}
           showsVerticalScrollIndicator={false}
           renderItem={({ item }) => (
             <Item
-              // data={{ status: item.status, description: item.description }}
               data={item}
               onStatusChange={() => console.log('changed')}
               onRemove={() => console.log('removed')}

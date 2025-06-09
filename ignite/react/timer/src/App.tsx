0361cc70e3b1284b53fe74ef2b21a69bac56a7e1
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-09 10:15:13 -0300

    react: global styles with styled components

--- ignite/react/timer/src/App.tsx
+++ ignite/react/timer/src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Button variant="primary" />
       <Button variant="secoundary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+      <h3>Test</h3>
     </ThemeProvider>
   )
 }

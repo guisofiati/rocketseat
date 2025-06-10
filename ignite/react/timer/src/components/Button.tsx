9442f21749c4c1fbdef997c6755c4e946737fee9
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-10 11:28:12 -0300

    react: react router dom, link between pages

--- ignite/react/timer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, type ButtonVariant } from "./Button.styles"
-
-interface Props {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = "primary" }: Props) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}

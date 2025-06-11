8db5df22afbc4658274d9c4bc684211c72ed4f96
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-11 16:35:52 -0300

    react: pages Timer and History created and styled, status dot component created

---
+++ ignite/react/timer/src/pages/Home/index.tsx
@@ -0,0 +1,49 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" list="taskSuggestions" type="text" placeholder="Dê um nome para o seu projeto" />
+          <datalist id="taskSuggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            step={5}
+            min={5}
+            max={60}
+            placeholder="00"
+          />
+          <span>minutos.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}

a6dcf930d7aeaaeac855a55d4e0e3b150d1f1fd8
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-11 21:43:53 -0300

    react native: useRoute hook to use route params

--- ignite/react-native/navigation/src/app/Product.tsx
+++ ignite/react-native/navigation/src/app/Product.tsx
@@ -4,13 +4,18 @@ import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { ButtonIcon } from "@/components/ButtonIcon";
 import { StackRoutesProps } from "@/routes/StackRoute";
+import { useRoute } from "@react-navigation/native";
 
-export function Product({ navigation, route }: StackRoutesProps<"product">) {
+type RouteParams = StackRoutesProps<"product">
+
+export function Product({ navigation }: StackRoutesProps<"product">) {
+  const { params } = useRoute<RouteParams["route"]>()
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <ButtonIcon name="arrow-circle-left" onPress={() => navigation.navigate("home")} />
-        <Title>Product {route.params?.id}</Title>
+        {/*<Title>Product {route.params?.id}</Title> */}
+        <Title>Product {params?.id}</Title>
       </Header>
     </View>
   )

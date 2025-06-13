d4068ea1325f5fdbcd4e5dfe1036d602851d4b95
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-13 12:36:40 -0300

    react: typing form with zod infer

--- ignite/react/timer/src/pages/Home/index.tsx
+++ ignite/react/timer/src/pages/Home/index.tsx
@@ -21,10 +21,7 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, "O ciclo precisa ser de no máximo de 60 minutos.")
 })
 
-interface NewCycleFormData {
-  task: string;
-  minutesAmount: number
-}
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
   // register -> função que recebe o name do input e retorna metodos desse input (onChange, onBlur...)

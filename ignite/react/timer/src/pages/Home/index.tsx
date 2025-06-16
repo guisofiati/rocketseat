ea92fcbbb89b6c2ec4fe51d0b543e39d4fd24f5c
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-16 11:08:05 -0300

    react: useEffect hook and countdown

--- ignite/react/timer/src/pages/Home/index.tsx
+++ ignite/react/timer/src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as zod from "zod"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Play } from "phosphor-react";
+import { differenceInSeconds } from "date-fns"
 import {
   CountdownContainer,
   FormContainer,
@@ -26,7 +27,8 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 interface Cycle {
   id: string;
   task: string;
-  minutesAmount: number
+  minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -44,19 +46,30 @@ export function Home() {
     }
   })
 
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        // setInterval nao é mt preciso, entao fazer por ex: setAmountSecondsPassed(state => state + 1)
+        // não seria muito viável
+        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+      }, 1000);
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const newCycle: Cycle = {
       id: String(new Date().getTime()),
       task: data.task,
-      minutesAmount: data.minutesAmount
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
     }
 
     setCycles(prevState => [...prevState, newCycle])
     setActiveCycleId(newCycle.id)
     reset() // volta os campos para os valores definidos no defaultValues do useForm
   }
-
-  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0

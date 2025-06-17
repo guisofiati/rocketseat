ea7df978565cf23d4155bfd31fb1cd2d90064fc4
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2025-06-17 15:10:59 -0300

    react: history list

--- ignite/react/timer/src/pages/Home/components/NewCycleForm/index.tsx
+++ ignite/react/timer/src/pages/Home/components/NewCycleForm/index.tsx
@@ -32,7 +32,7 @@ export function NewCycleForm() {
         id="minutesAmount"
         type="number"
         step={5}
-        min={1}
+        min={5}
         max={60}
         placeholder="00"
         disabled={!!activeCycle}
